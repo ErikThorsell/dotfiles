2e829622adc3491c8c522908def935bfea2311f3
Erik Thorsell <erik@thorsell.io>
2021-10-27 09:29:25 +0200

    Add download of asdf

--- setup.sh
+++ setup.sh
@@ -9,22 +9,29 @@ done;
 echo "~~oo##> SETTING UP DOTFILES <##oo~~"
 
 echo "Creating symlinks in $HOME/"
+
 echo " $ ln -s $PWD/bash/bashrc $HOME/.bashrc"
 ln -s $PWD/bash/bashrc $HOME/.bashrc
+
 echo " $ ln -s $PWD/bash/profile $HOME/.profile"
 ln -s $PWD/bash/profile $HOME/.profile
+
 echo " $ ln -s $PWD/vim/init.vim $HOME/.vimrc"
 ln -s $PWD/vim/init.vim $HOME/.vimrc
+
 echo " $ ln -s $PWD/vim $HOME/.vim"
 ln -s $PWD/vim $HOME/.vim
+
 echo " $ ln -s $PWD/git/gitconfig_user $HOME/.gitconfig"
 ln -s $PWD/git/gitconfig_user $HOME/.gitconfig
 
 echo "Creating undodir for vim"
 mkdir -p $HOME/.vim/undodir
 
+echo "~~oo##> INSTALLING asdf <##oo~~"
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.8.1
+
 echo "~~oo##> ALMOST READY <##oo~~"
 echo " 1. Don't forget to update user information in $HOME/.gitconfig"
 echo " 2. Install all vim plugins by executing :PlugInstall in vim"
 
-echo "~~oo##> ############ <##oo~~"
