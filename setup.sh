611bebe7a01d152c0674af535f9148aa8c09ecc8
Erik Thorsell <erik@thorsell.io>
2021-10-28 06:37:39 +0200

    Backup dotfiles before creating symlinks

--- setup.sh
+++ setup.sh
@@ -7,21 +7,28 @@ while [[ $ready != 'y' ]]; do
 done;
 
 echo "~~oo##> SETTING UP DOTFILES <##oo~~"
+echo "All files/directories will be backed up to $HOME/<name>.bak"
+echo "If everything works, just remove the .bak-files"
 
 echo "Creating symlinks in $HOME/"
 
+[ -f $HOME/.bashrc ] && mv $HOME/.bashrc $HOME/bashrc.bak
 echo " $ ln -s $PWD/bash/bashrc $HOME/.bashrc"
 ln -s $PWD/bash/bashrc $HOME/.bashrc
 
+[ -f $HOME/.profile ] && mv $HOME/.profile $HOME/profile.bak
 echo " $ ln -s $PWD/bash/profile $HOME/.profile"
 ln -s $PWD/bash/profile $HOME/.profile
 
+[ -f $HOME/.vimrc ] && mv $HOME/.vimrc $HOME/vimrc.bak
 echo " $ ln -s $PWD/vim/init.vim $HOME/.vimrc"
 ln -s $PWD/vim/init.vim $HOME/.vimrc
 
+[ -f $HOME/.vim ] && mv $HOME/.vim $HOME/vim.bak
 echo " $ ln -s $PWD/vim $HOME/.vim"
 ln -s $PWD/vim $HOME/.vim
 
+[ -f $HOME/.gitconfig ] && mv $HOME/.gitconfig $HOME/gitconfig.bak
 echo " $ ln -s $PWD/git/gitconfig_user $HOME/.gitconfig"
 ln -s $PWD/git/gitconfig_user $HOME/.gitconfig
 
