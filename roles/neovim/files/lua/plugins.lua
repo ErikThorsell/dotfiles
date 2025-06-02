97de76e932e98872a2a3d901237a80d299d7e48e
Erik Thorsell <erik@thorsell.io>
2025-06-02 07:34:10 +0200

    Use tofu_fmt to format terraform code in neovim

--- roles/neovim/files/lua/plugins.lua
+++ roles/neovim/files/lua/plugins.lua
@@ -462,6 +462,7 @@ require("lazy").setup({
 			end,
 			formatters_by_ft = {
 				lua = { "stylua" },
+				terraform = { "tofu_fmt" },
 				-- Conform can also run multiple formatters sequentially
 				-- python = { "isort", "black" },
 				--
