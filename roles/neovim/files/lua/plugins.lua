5409280c7b6d7b618b98ccd01dd2a1f4ba26ef47
Erik Thorsell <erik@thorsell.io>
2026-01-09 11:09:29 +0100

    Fix neovim treesitter plugin

--- roles/neovim/files/lua/plugins.lua
+++ roles/neovim/files/lua/plugins.lua
@@ -600,36 +600,66 @@ require("lazy").setup({
 	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		main = "nvim-treesitter.configs", -- Sets main module to use for opts
-		-- [[ Configure Treesitter ]] See `:help nvim-treesitter`
-		opts = {
-			ensure_installed = {
-				"bash",
-				"c",
-				"diff",
-				"html",
-				"lua",
-				"luadoc",
-				"markdown",
-				"markdown_inline",
-				"query",
-				"vim",
-				"vimdoc",
-			},
-			-- Autoinstall languages that are not installed
-			auto_install = true,
-			highlight = {
-				enable = true,
-				-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-				--  If you are experiencing weird indenting issues, add the language to
-				--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-				additional_vim_regex_highlighting = { "ruby" },
-			},
-			indent = {
-				enable = true,
-				disable = { "ruby", "markdown", "markdown_inline" },
-			},
-		},
+		config = function()
+			-- modern entrypoint
+			local ok, ts = pcall(require, "nvim-treesitter.configs")
+			if ok then
+				ts.setup({
+					ensure_installed = {
+						"bash",
+						"c",
+						"diff",
+						"html",
+						"lua",
+						"luadoc",
+						"markdown",
+						"markdown_inline",
+						"query",
+						"vim",
+						"vimdoc",
+					},
+					auto_install = true,
+					highlight = {
+						enable = true,
+						additional_vim_regex_highlighting = { "ruby" },
+					},
+					indent = {
+						enable = true,
+						disable = { "ruby", "markdown", "markdown_inline" },
+					},
+				})
+				return
+			end
+
+			-- fallback for layouts where configs module isn't present
+			local ok2, cfg = pcall(require, "nvim-treesitter.config")
+			if ok2 and cfg.setup then
+				cfg.setup({
+					ensure_installed = {
+						"bash",
+						"c",
+						"diff",
+						"html",
+						"lua",
+						"luadoc",
+						"markdown",
+						"markdown_inline",
+						"query",
+						"vim",
+						"vimdoc",
+					},
+					auto_install = true,
+					highlight = {
+						enable = true,
+						additional_vim_regex_highlighting = { "ruby" },
+					},
+					indent = {
+						enable = true,
+						disable = { "ruby", "markdown", "markdown_inline" },
+					},
+				})
+			end
+		end,
 	},
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
